a40b7294ae0224790e85f568db55d3c399dee797
Pedro Lucas de Sousa Ribeiro <devpedro37@gmail.com>
2025-10-25 14:12:40 -0300

    Finalizando a sessao de formatacao captura, validacao e formatacao do valor passado pelo usuario

--- index.js
+++ index.js
@@ -11,8 +11,27 @@ amount.oninput = () =>{
     //removendo as letras
     let value = amount.value.replace(/\D/g, "")
 
+
+    //TRANSFORMANDO O VALOR EM CENTAVOS
+    value = Number(value) / 100 //transformando em centavos
+
     //devolvendo só os números para o front
-    amount.value = value
+    amount.value = formatarMoedaBRL(value)
 
 
 } //OBESERVA CONTEUDOS DO INPUT
+
+
+//FUNÇÕES
+
+//FORMATAR A MOEDA PARA pt-br
+function formatarMoedaBRL(value){
+    //FORMATA O VALOR NO PADRÃO BRL (real brasileiro)
+    value = value.toLocaleString("pt-BR", {
+        style: "currency",
+        currency: "BRL"
+    })
+
+    //RETORNA O VALOR FORMATADO
+    return value
+}
